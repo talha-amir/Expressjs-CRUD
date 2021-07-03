9d53055a2393c60887cd203c042be6ad589a677e
Talha Amir <talha.amir9225@gmail.com>
2021-07-03 19:21:07 +0500

    Port Updated

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ app.use(bodyParser.json())
 require('./app/routes/customer.routes.js')(app);
 
 // Create a Server
-var server = app.listen(8081, function () {
+// var server = app.listen(8081, function () {
 
-    var host = server.address().address
-    var port = server.address().port
+//     var host = server.address().address
+//     var port = server.address().port
 
-    console.log("App listening at http://%s:%s", host, port)
+//     console.log("App listening at http://%s:%s", host, port)
 
-})
+// })
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+});
