c6860878cd344df213cd4551b032bdce573843f7
Talha Amir <talha.amir9225@gmail.com>
2021-07-05 22:30:08 +0500

    Typo Fixed

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.use(bodyParser.json())
 
 require('./app/routes/customer.routes.js')(app);
 
-// Create a Server
-
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(server_port, server_host, function () {
